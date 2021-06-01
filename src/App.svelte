c32ff35782e65bfcc019e2f2ef69435928eb750e
Lev Savolsky <lev.savolskyy@gmail.com>
2021-06-01 20:01:08 +0300

    Новый токен

--- src/App.svelte
+++ src/App.svelte
@@ -9,7 +9,7 @@
   const backendUrl = 'https://tolmatch-backend.herokuapp.com/'
   let wordsState = []
   let idState
-  let token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI3NmI4MjA4OWJiNzJkMTA3NGMzMWIwNmQ1YWVmMjE0NDg4NjhjYjZkZTUxZDM4NTE0ZDY1ZjZlYTJmOTgxOTlhNTM5YmU5MjcwMDQyNjI5OCIsImF1ZCI6IlZQUyIsImV4cCI6MTYyMjIzNTkwMSwiaWF0IjoxNjIyMTQ5NDkxLCJpc3MiOiJLRVlNQVNURVIiLCJ0eXBlIjoiQmVhcmVyIiwianRpIjoiODliNWZiMzMtN2ExMi00N2Y2LThjOGEtYzJjNjgzN2ZhZTA5Iiwic2lkIjoiNTk2ZDU3OWMtMGM2MS00YTU4LTk3NWEtMjVlNGU0ZDY2YmFmIn0.BUOWZjyz5LwcVdEJ_M9YrjdT9ZV721rfHoesPghIMc6cNytNaHBnmsGBpiNYomt_5JMcEVg7cBE-uzCQ6vuf7c4KmZ8cW_3-h928WRiGjYPmPPOUcS7ai0wT1PcBxgCTl7v6lw7_6HCiiDHh6x4LW2L9WobPUmKXwxsYoJietdFiHmtKApRZc3f3zOpqzkHGSjId_W7vlOGlFi4J8IVJOFyxujUyQ9_Ks06PMiSiojPfUq9O_oCmUpb1j_uchSljbvYWFbwgZ-fnI33o4fFFzo8VahL7GOOAtM7eIjWuqWJKdhjUrZc_DEO37K25m1EzOX9Ku4EYCEAn9upVlXugi8gssvBfZZUXMLZV_tCT_3j26iWiRSKB7WwVUQsLNJ7ixkEfqUB2wJJ-P1AabNgsOk6C_7orAytw8AH8YLXHf-MF9vweorgDY8abhCEcKSQwnbklhq7ptKf4XvrQCtlQadT94vmKpen9ULEAbLN--PrBfM63W2U1Qbz6-FVbt6F0ChbHb5liIROzdAU9LnQ3aiybdcL4WnbCLfLFq4DueI8EkIo4Se1uzPHfCjjWYk7yix8s5NLUzG5JyvG6EQ6il57ti-J9IYs4MScdKvd1sET0tBBKTOR-Y5qkrW-FtGhtrsW4rEUcHGdNqTNavIDbompR8C3WzGLP1O0T6GCuewI';
+  let token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI3NmI4MjA4OWJiNzJkMTA3NGMzMWIwNmQ1YWVmMjE0NDg4NjhjYjZkZTUxZDM4NTE0ZDY1ZjZlYTJmOTgxOTlhNTM5YmU5MjcwMDQyNjI5OCIsImF1ZCI6IlZQUyIsImV4cCI6MTYyMjY1MzAwMCwiaWF0IjoxNjIyNTY2NTkwLCJpc3MiOiJLRVlNQVNURVIiLCJ0eXBlIjoiQmVhcmVyIiwianRpIjoiZDRmMWU5ZjctMjY5Mi00ZjhiLWI2ZTQtZTAwOGQ3ZTQ1ZDViIiwic2lkIjoiYzhiZWYzZTUtNWI0NS00OTE5LWJkNmUtZTU1ODIwOGM4NjVlIn0.APCa4-ViqJGnTFJFRQBwAREb6T5Im2nHqkP1-Lu4DywT2QqdwLtxKU4ZadolijxMpiZMzP6_-W94jVoL0U2HN3uT1iH3cX9grBIE9Iir_s0UM3tIOh7_0uIju6WxaqtysDXOdVyhuEyRRnx1IuxQISE6s6FpohJ0d12lQ8F29hx6eaGwsLn1X9MQiYDViy1ES6TTYW2wc3P28TUxCTSKoz8YbWRMjUf5RYHNDuZWV_rojW0ld5FY94qK82-KS38iWqsGsR-ook4AV2P99upQ5KdsZtSpr7TVvo6qEe2KnKI76agAFqT4Fi8EEHS-YyeVDQUL6JMSRjnz2OiXSblA2A0wNhkPg8caCc-Rta8D1K3j-hvgohJR3H18qJCZksx06OzaMGEXOPCUYgwGM5NUNKLrAZr3cF8rgdgjxYu7RTZe4mhxWgX2M3RBCk4WfQfujw26bNB6jEjhJZABO-8pLuiZW4xgF3U9mG5OBnaxZhJEus3K6gMYoN_MJBqIrY7tUeCTNzPeCcZMUWUTnQ_60SJUSYTpeViFubO79b9v2yOC3o6z7OTadRnmVYN9ESk92vX9iUS8hzwHG1JnwHfollQI6BCl_jEDKvau1embqLvdhdwzIVYjCLJtlKVWlNVC6FJ2VY9YcD76Y3N5sjT4GEHWqFvaigfuOkOYoFkyEJk';
   //let token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhMzM2YmEwMjU3MzEyN2RkMDY1MmYxNDRmZDI5NmQwNWViNmIyZTk5MTJhMmVlMDQ5NzAyMWE4ZGE0NDVjM2E5NTM5YmU5MjcwMDQyNjI5OCIsImF1ZCI6IlZQUyIsImV4cCI6MTYyMjAzNDIzNCwiaWF0IjoxNjIxOTQ3ODI0LCJpc3MiOiJLRVlNQVNURVIiLCJ0eXBlIjoiQmVhcmVyIiwianRpIjoiNDkxZTRjMjUtOGE0Ni00MTIxLTllMDktOGNkYWVmYjg0NTU0Iiwic2lkIjoiYjI1MmRmMzktY2JkNy00NmY5LThhZWEtYjAyNTNjMzA4MTI2In0.UJ-GIOJ0JqNw1kBDe_9T42YXBZL6LrmQ9dWMOKiRG6qWBxSPrwYZD1LS32KENvLv7NvCURs_nTM6J622vooQ-EM366yj1UiQQ_sMmw8XPKlq1q46GIfM9nF0ngu_Lzny4GDfxytsA_T-DdQby9g1HdqH1TWnbOhUA3gz7S2wL-esWF2IENDJI01Yx_VR141fi9fc8eM0PM_gHWJOsL7v8ZHFMBcDhT4zRsHPhgXxKJCApL98xzu9akpXjrrJ_5F8gvaZ-mKSVTsd3aAZyzR4Eh9JuVmhyCekD3BohwF61Tn6wse7FjTYkM5yzqM8BY8CyIchAQUPg3yBS3z9E1K5jn4WiDBFpUVI7vrkZOCZspa7l64jagUyCASoVHLni-PArZBFsn9ztPKA0rh4znTh5Hz8JMDktzT6bG7QYe911DrWtTL0dQkmz73KHEzaH1_TWt2C3Qbaht_i78qGK7Oo5oG4WPWI_wv9Keyg8_SCsk6I9oMSRVd-5onWO1cqfR8QUo4wuSJmUd84lhdfw_OEyWb8wFDmuGJemj_0zYRBnXdG_XV8AR1OLe1GZ9Ulv4sV3Ib0KSxMGYrGecj3Dy2BdQubUmOl7Zo6GY9X7_xgDSHqEw4h41lqtRRQQxew9q_aRACbdWJfrAPjxGOPf-PDmcis8PYsH9URfbGyJNbXYu8';
   let initPhrase = 'Включи квиз-переводчик'; 
 
@@ -32,12 +32,12 @@
   let assistant;
   onMount(() => {
      const init = () => {
-       //return createSmartappDebugger({
-       //  token,
-       //  initPhrase,
-       //  getState,
-       //  settings: {debugging: false}
-       //})
+      //  return createSmartappDebugger({
+      //   token,
+      //   initPhrase,
+      //   getState,
+      //   settings: {debugging: false}
+      //  })
       return createAssistant({getState});
      }
     assistant = init();
@@ -74,6 +74,7 @@
     if (ind === rightInd) {
       message = '';
       document.getElementById(ind.toString()).style.backgroundColor = 'green'
+      
       promise = newGame();
     } else {
       document.getElementById(ind.toString()).style.backgroundColor = 'red'
