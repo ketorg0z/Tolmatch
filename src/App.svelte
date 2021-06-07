667b5e70b6f1750ef2443a141b1f191f518c1b93
ketorg0z <lev.savolsky@gmail.com>
2021-06-07 19:37:50 +0300

    токен

--- src/App.svelte
+++ src/App.svelte
@@ -9,7 +9,7 @@
   const backendUrl = 'https://tolmatch-backend.herokuapp.com/'
   let wordsState = []
   let idState
-  let token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI3NmI4MjA4OWJiNzJkMTA3NGMzMWIwNmQ1YWVmMjE0NDg4NjhjYjZkZTUxZDM4NTE0ZDY1ZjZlYTJmOTgxOTlhNTM5YmU5MjcwMDQyNjI5OCIsImF1ZCI6IlZQUyIsImV4cCI6MTYyMjkxNjI2NiwiaWF0IjoxNjIyODI5ODU2LCJpc3MiOiJLRVlNQVNURVIiLCJ0eXBlIjoiQmVhcmVyIiwianRpIjoiMTQ5NmQ0NWMtYzg5MC00NDk5LWExNjctZWM5OGU5ODYwYTBkIiwic2lkIjoiMjQzYzJkMTktNTI5Ni00MGJjLTg4M2ItNWFjMWFjMmYzMzk3In0.jH8noU3F1U-O3HRXRmX8pEzlLEitbAY1wsGueGIyA3KPc95vi554tjpmsDVIQ0coZRDja85gk7DJvg8V3IhVwb7KEe-NyhvUDGZdhwg1y214BAjLWpY58zxVqol2mwKTrHXWmg07SpLdHC4TsgpkXwxOK4tnd34OGf3aDyXgJq-aasOl7gusRcVSSj9oL0C7NJ_Q5N28C4yUaSFh_FN56LPdnUIy5__rdVYE-w-Ld8Zi-2slyArxwOrJFQ0EEayAqMR4v_zG9BhpCSMPZGc65MKeGiX0_f9eleSvA22ua-bBE-f2UURYUw_HsF2xySGacaJdgn-dr1mp4wS0iBS_adQ7RueLaFgoBTn53zzfNMaDP5FX-hoFknrs3Sh_57dfcBz37cw_nNLAxzP0sytYhGHpb276YDaZAZ3y2srhB7N5d3D-8QWduLq7l5CrHmXmejeP-230otLPZr5zkZt9iX9Aapy2vbOZ26xqV7S6UIkpCl-c2cBrJVMFuXh4xWeEdPuhV1O4nixmp6p3UZqK6B6QWkVaaJ6qh6rmnNx-_2IdEgdy63OYVKcY1xyENrOBWUckhwWBqYsWQhdEx07jrKQTOycyeQz7Ck-6JIYHiV9yP0xuNgi9hV66qLBvv7WGJyZJlYdcquIc7mH1uHcyZQwrahR-twiLtZP1cN0UZgw';
+  let token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI3NmI4MjA4OWJiNzJkMTA3NGMzMWIwNmQ1YWVmMjE0NDg4NjhjYjZkZTUxZDM4NTE0ZDY1ZjZlYTJmOTgxOTlhNTM5YmU5MjcwMDQyNjI5OCIsImF1ZCI6IlZQUyIsImV4cCI6MTYyMzE2ODgxMSwiaWF0IjoxNjIzMDgyNDAxLCJpc3MiOiJLRVlNQVNURVIiLCJ0eXBlIjoiQmVhcmVyIiwianRpIjoiZDE4MjEwOGYtNzRmMi00ZTZjLTljYjMtNzM5Yjg5MDVlMjcwIiwic2lkIjoiMmYyNjlmYmItZmI2Yi00Y2NjLThkODYtODI0MGFjYjRkY2QyIn0.Uq-3vzElco6imp4AYu_O4wJJqWNrYiN7Mwe7_P_mo8uHx0KBt0EQyEp4kCHRjnMWC3_w2HCE7KcT8pmZrfKxrryaKlF9UcTHhsQTKMFeuc-kCDJqQdlw0evXKvAf_ADWoGiE3x5To9UG7hglxi2XQV-7TdzWrZxbevfX-8m4_YMtD1Awtsf81ptLtZYucw5i6nHkSLdWqjU81WAFcu926kTEtsxtN6iEZs4VGgO74fT7Vr-gNSiUAsBetNEZeHl-40h10WedQQ0jE6jQnnU0PKV_-7UMhIxt3LeJFiyDDmVHOXCjdVaqFgEQgX6zM1b5QLWiGxAjdxQKIFNYWjToCvYnv5w4Bk_hZjE9c7IHmMot0PZa_AtCUh6JdpE18LNi3BEeH4KV-Y_-ItNLJSOGabwFNTu5-2zK16fEGtKS-FS9sMVBrWtqGGBWZ-XiSEaQLgY741GmWL-nO73nudOqPYW07RQc7IkxoQwM3clA7oZ36_Or24fQuzvEwZiaA0IiFQ1dLb1IATOkPu3AqiP89UHK9BiRriZX0bn5JE_wPw4NPOSa9NLeaLDhtZadpCzpxMZ1dxicSImWtLQTwgEd5I9qere5zAxoiOg5104hbGjE4wAXDUb056GsecOEa_OIJJh1C1me0EOi2OaPlXNf3Juidz1DVq-7UMzTCg2A2BY';
   let initPhrase = 'Включи толмач'; 
 
   function getState() {
@@ -31,12 +31,12 @@
   let assistant;
   onMount(() => {
       const init = () => {
-    //    return createSmartappDebugger({
-    //     token,
-    //     initPhrase,
-    //     getState,
-    //     settings: {debugging: false}
-    //    })
+      //  return createSmartappDebugger({
+      //   token,
+      //   initPhrase,
+      //   getState,
+      //   settings: {debugging: false}
+      //  })
        return createAssistant({getState});
       }
     assistant = init();
